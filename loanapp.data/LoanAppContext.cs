1caff2c534be0589082760b6d3ca787e101b12bd
nskalu <Nollykalz@1>
2025-09-10 06:20:38 +0100

    feat: add dbcontext and factory

---
+++ loanapp.data/LoanAppContext.cs
@@ -0,0 +1,45 @@
+﻿using loanapp.data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace LoanApp.Data
+{
+    public class LoanAppContext : DbContext
+    {
+        public LoanAppContext(DbContextOptions<LoanAppContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<LoanApplication> LoanApplications { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<LoanApplication>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.ApplicantName)
+                      .IsRequired()
+                      .HasMaxLength(200);
+
+                entity.Property(e => e.LoanAmount)
+                      .HasColumnType("decimal(18,2)");
+
+                entity.Property(e => e.LoanTerm)
+                      .IsRequired();
+
+                entity.Property(e => e.InterestRate)
+                      .HasColumnType("decimal(5,2)");
+
+                entity.Property(e => e.LoanStatus)
+                      .IsRequired();
+
+                entity.Property(e => e.ApplicationDate)
+                      .HasColumnType("datetime")
+                      .IsRequired();
+            });
+        }
+    }
+}
---
+++ loanapp.data/LoanAppContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+
+namespace LoanApp.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<LoanAppContext>
+    {
+        public LoanAppContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<LoanAppContext>();
+
+            optionsBuilder.UseMySql(
+                "server=localhost;database=LoanAppDb;user=ngozi;password=123456",
+                new MySqlServerVersion(new Version(8, 0, 36)) 
+            );
+
+            return new LoanAppContext(optionsBuilder.Options);
+        }
+    }
+}
