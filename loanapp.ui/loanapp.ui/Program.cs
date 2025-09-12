ced4a71bdb8f42a3c756b65ea9810868b76df2eb
nskalu <Nollykalz@1>
2025-09-12 05:00:06 +0100

    feat: create ui layer and add new page to get list of applications

---
+++ loanapp.ui/loanapp.ui/Program.cs
@@ -0,0 +1,53 @@
+using loanapp.application.Commands.Loans;
+using loanapp.ui.Components;
+using loanapp.ui.Client;
+using LoanApp.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddDbContext<LoanAppContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+    ));
+
+// Register MediatR
+builder.Services.AddMediatR(m =>
+    m.RegisterServicesFromAssembly(typeof(CreateLoanApplication.Handler).Assembly)
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(loanapp.ui.Client._Imports).Assembly)
+    ;
+
+app.Run();
